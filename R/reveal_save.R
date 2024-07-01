f57c1d47e05c34eb544677bd34333bb92d8286fc
Weverthon Machado <11988227+weverthonmachado@users.noreply.github.com>
2024-07-01 15:19:20 +0200

    improve saving

--- R/reveal_save.R
+++ R/reveal_save.R
@@ -10,14 +10,18 @@ reveal_save <- function(plot_list, basename = "plot", ...) {
 
   cli::cli_h2("Saving incremental plots")
 
+  paths <- c()
   for (i in 1:length(plot_list)) {
     suffix <- ifelse(i == length(plot_list),
                      paste0("_", i, "_full"),
                      paste0("_", i))
     filename <- paste0(basename, suffix, ".png")
-
-    ggplot2::ggsave(filename, plot_list[[i]], ...)
-    cli::cli_alert_success("{filename}")
+    
+    filename <- ggplot2::ggsave(filename, plot_list[[i]], ...)
+    paths <- c(paths, filename)
+    cli::cli_alert_success("{.file {filename}}")
   }
+
+  invisible(paths)
 
 }
